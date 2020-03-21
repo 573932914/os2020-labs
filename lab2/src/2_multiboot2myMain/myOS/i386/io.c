97f92bf808909164d6123ef7714ee6718198a598
轻轻的飘 <53417909+573932914@users.noreply.github.com>
2020-03-21 19:33:31 +0800

    E

--- lab2/src/2_multiboot2myMain/myOS/i386/io.c
+++ lab2/src/2_multiboot2myMain/myOS/i386/io.c
@@ -1,4 +1,5 @@
 /* IO operations */
+//使用内嵌汇编实现对给定地址的读写操作
 unsigned char inb(unsigned short int port_from){
     unsigned char value;
     __asm__ __volatile__ ("inb %w1,%b0":"=a" (value):"Nd" (port_from));
